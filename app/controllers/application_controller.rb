092e901137612e13b1e3b922011613eb54266a64
Facundo Diaz Martinez <fdiaz@fu.do>
2023-05-26 01:12:51 -0300

    feat: simulate current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def current_user
+    # TODO: Replace this with a real authentication system
+    # In order to keep this example simple, we're just going to hardcode a
+    # single user that will be returned regardless of the request.
+    @current_user ||= Account.first
+  end
 end
