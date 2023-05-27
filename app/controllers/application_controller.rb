475ff21d377e4696da6bfa838802b59d90768be5
dmfacundo <eng.dmfacundo@gmail.com>
2023-05-27 17:25:42 -0300

    feat: allow to simulate different user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::API
     # TODO: Replace this with a real authentication system
     # In order to keep this example simple, we're just going to hardcode a
     # single user that will be returned regardless of the request.
-    @current_user ||= Account.first
+    @current_user ||= Account.find_by(id: request.headers["HTTP_ACCOUNT_ID"]) || Account.first
   end
 end
